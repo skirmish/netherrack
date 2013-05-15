3598b0937570f7a22492cc9070a5633c09414849
Thinkofdeath <purggames@gmail.com>
2013-05-15 19:19:26 +0100

    Add error checking to protocol handling

--- protocol/read.go
+++ protocol/read.go
@@ -16,13 +16,19 @@ func (c *Conn) readBool() bool {
 
 func (c *Conn) ReadUByte() byte {
 	data := make([]byte, 1)
-	c.Read(data)
+	_, err := c.Read(data)
+	if err != nil {
+		runtime.Goexit()
+	}
 	return data[0]
 }
 
 func (c *Conn) readUByte() byte {
 	data := make([]byte, 1)
-	c.Read(data)
+	_, err := c.Read(data)
+	if err != nil {
+		runtime.Goexit()
+	}
 	return data[0]
 }
 
@@ -32,7 +38,10 @@ func (c *Conn) readByte() int8 {
 
 func (c *Conn) readUShort() uint16 {
 	data := make([]byte, 2)
-	c.Read(data)
+	_, err := c.Read(data)
+	if err != nil {
+		runtime.Goexit()
+	}
 	return binary.BigEndian.Uint16(data)
 }
 
@@ -42,25 +51,37 @@ func (c *Conn) readShort() int16 {
 
 func (c *Conn) readInt() int32 {
 	data := make([]byte, 4)
-	c.Read(data)
+	_, err := c.Read(data)
+	if err != nil {
+		runtime.Goexit()
+	}
 	return int32(binary.BigEndian.Uint32(data))
 }
 
 func (c *Conn) readLong() int64 {
 	data := make([]byte, 8)
-	c.Read(data)
+	_, err := c.Read(data)
+	if err != nil {
+		runtime.Goexit()
+	}
 	return int64(binary.BigEndian.Uint64(data))
 }
 
 func (c *Conn) readFloat() float32 {
 	data := make([]byte, 4)
-	c.Read(data)
+	_, err := c.Read(data)
+	if err != nil {
+		runtime.Goexit()
+	}
 	return math.Float32frombits(binary.BigEndian.Uint32(data))
 }
 
 func (c *Conn) readDouble() float64 {
 	data := make([]byte, 8)
-	c.Read(data)
+	_, err := c.Read(data)
+	if err != nil {
+		runtime.Goexit()
+	}
 	return math.Float64frombits(binary.BigEndian.Uint64(data))
 }
 
